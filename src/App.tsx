6eba1fb692f78134ee5fc199926c515f3d1a2129
Alvin Leung <alvinleung2009@gmail.com>
2023-03-03 16:01:06 -0800

    working chatbot interface:

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,74 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 // import { useDebounce, useDebouncedCallback } from "use-debounce";
 import { useLocalStorage, useDebounce } from "usehooks-ts";
-
-const TOKEN =
-  "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1UaEVOVUpHTkVNMVFURTRNMEZCTWpkQ05UZzVNRFUxUlRVd1FVSkRNRU13UmtGRVFrRXpSZyJ9.eyJodHRwczovL2FwaS5vcGVuYWkuY29tL3Byb2ZpbGUiOnsiZW1haWwiOiJhbHZpbmxldW5nMjAwOUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZ2VvaXBfY291bnRyeSI6IkNBIn0sImh0dHBzOi8vYXBpLm9wZW5haS5jb20vYXV0aCI6eyJ1c2VyX2lkIjoidXNlci1XZ3ptMmhBM0dMU1U1QUx2NVdLQ2xFQ3QifSwiaXNzIjoiaHR0cHM6Ly9hdXRoMC5vcGVuYWkuY29tLyIsInN1YiI6Imdvb2dsZS1vYXV0aDJ8MTA4MDQ2NTUwNzc0NDU0NTE3MzEzIiwiYXVkIjpbImh0dHBzOi8vYXBpLm9wZW5haS5jb20vdjEiLCJodHRwczovL29wZW5haS5vcGVuYWkuYXV0aDBhcHAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTY3NjY2OTY3NCwiZXhwIjoxNjc3ODc5Mjc0LCJhenAiOiJUZEpJY2JlMTZXb1RIdE45NW55eXdoNUU0eU9vNkl0RyIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwgbW9kZWwucmVhZCBtb2RlbC5yZXF1ZXN0IG9yZ2FuaXphdGlvbi5yZWFkIG9mZmxpbmVfYWNjZXNzIn0.KLDAXKXSw-ZS8Y-aRJvyryqjxEOVkJUSC1ObsPGVXQeahvSZgX0h3alShlB0qV_ynY9X1IDsSRTHlEfl8s3DnlIrmppjR3k2AeE4LjFHwpi2ULWnVPnqWMUZCdQ1dMaiKEjtOFz0RzinuvnIifILRKEJib4gda7o0yqh0B7qaA3IDXSxinc7S7yYVnJPpa6bM0cKNqwo3IIindAZwiU6nOVtSXu98r-IAUhCfAjewA0tjxM4REWhuyeL345P09OUxHJzMroNbxaElG9S_fzR6NTmoUu4j8Bg3NN1pQFGIc5AnKK2LyIZXPOJB5kKj6kOfAiD7IJw_QFZ_lGw2PbvNg";
+import { createMessageStore, MessageRole } from "./chatapi/MessageStore";
+import { useMessageStore } from "./hooks/useMessageStore";
 
 const App: React.FC = () => {
-  const [queryInput, setQueryInput] = useLocalStorage("query", "");
+  const [prompt, setPrompt] = useLocalStorage("prompt", "");
+  const { messages, addMessage } = useMessageStore();
 
-  const DEBOUNCE_TIMER = 1000;
-  const query = useDebounce(queryInput, DEBOUNCE_TIMER);
-
-  const [result, setResult] = useState("");
-  const [isFindingAns, setIsFindingAns] = useState(false);
+  const [isWaitingResponse, setIsWaitingResponse] = useState(false);
 
   const submitPrompt = useCallback(() => {
-    if (query === "") {
-      setIsFindingAns(false);
+    // const SERVER_URL = `http://${window.location.hostname}:3000`;
+
+    if (isWaitingResponse) return;
+
+    if (prompt === "") {
+      setIsWaitingResponse(false);
       return;
     }
 
-    async function beginQuery() {
-      if (!result) {
-      }
+    async function promptGPTFromServer(message: string) {
+      const requestBody = {
+        content: message,
+      };
 
-      setIsFindingAns(false);
-      setQueryInput(""); // empty the query
+      const response = await fetch("/prompt", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        body: JSON.stringify(requestBody),
+      });
+
+      return await response.json();
     }
 
-    setIsFindingAns(true);
-    beginQuery();
-  }, [query]);
+    // start the async prompting process
+    setIsWaitingResponse(true);
+    (async function () {
+      // add to local entry
+      addMessage(MessageRole.USER, prompt);
+
+      // prompt chat gpt
+      const response = await promptGPTFromServer(prompt);
+      addMessage(MessageRole.ASSISTANT, response.content);
+
+      // done prompting
+      setIsWaitingResponse(false);
+      setPrompt(""); // empty the prompt
+    })();
+  }, [prompt]);
 
   return (
     <div className="flex flex-col bg-gray-800 text-gray-300">
       <div className="m-4  min-h-screen">
         <div className="mt-4">
           <input
-            name="query"
+            disabled={isWaitingResponse}
+            name="prompt"
             className="w-full p-4 text-4xl block bg-gray-700 rounded-xl outline-none"
             placeholder="Ask a Question"
-            onChange={(e) => setQueryInput(e.target.value)}
+            onChange={(e) => setPrompt(e.target.value)}
             onKeyDown={(e) => {
               if (e.key === "Enter" && !e.shiftKey) {
                 // submit
@@ -48,22 +76,16 @@ const App: React.FC = () => {
                 e.preventDefault();
               }
             }}
-            value={queryInput}
+            value={prompt}
           />
         </div>
-        <div>
-          {isFindingAns ? (
-            <div>Loading</div>
-          ) : (
-            <div>
-              {result !== "" && (
-                <div className="w-full mt-4 p-2 text-4xl block bg-slate-100 rounded">
-                  <div className="opacity-30 text-sm uppercase">Answer</div>
-                  <div>{result}</div>
-                </div>
-              )}
+        <div className="flex flex-col-reverse">
+          {messages.map((message, index) => (
+            <div className="pt-4" key={index}>
+              <div className="text-normal opacity-50">{message.role}</div>
+              <div className="text-2xl">{message.content}</div>
             </div>
-          )}
+          ))}
         </div>
       </div>
     </div>
---
+++ src/hooks/useMessageStore.ts
@@ -0,0 +1,21 @@
+import { useMemo, useState } from "react";
+import {
+  createMessageStore,
+  Message,
+  MessageRole,
+} from "../chatapi/MessageStore";
+
+export function useMessageStore() {
+  const messageStore = useMemo(() => createMessageStore(), []);
+
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const addMessage = (role: MessageRole, content: string) => {
+    messageStore.add(role, content);
+    setMessages([...messageStore.getRecentMessages()]);
+  };
+  return {
+    messages,
+    addMessage,
+  };
+}
